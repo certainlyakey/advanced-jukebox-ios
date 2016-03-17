d1d24878468245b520236274fca7afebae40f0c0
AleksandrBeliaev <aleksandr.beliaev@exove.ee>
2016-03-17 02:00:08 +0200

    added list screen

---
+++ tutorial/SongTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  SongTableViewCell.swift
+//  tutorial
+//
+//  Created by AleksandrBeliaev on 17/03/16.
+//  Copyright © 2016 AleksandrBeliaev. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+	// MARK: Properties
+
+	@IBOutlet weak var name: UILabel!
+	@IBOutlet weak var photoImageView: UIImageView!
+	@IBOutlet weak var ratingControl: RatingControl!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ tutorial/SongTableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  SongTableViewController.swift
+//  tutorial
+//
+//  Created by AleksandrBeliaev on 17/03/16.
+//  Copyright © 2016 AleksandrBeliaev. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewController: UITableViewController {
+	
+	var songs = [Song]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		loadSampleSongs()
+    }
+	
+	
+	func loadSampleSongs() {
+		
+		let photo1 = UIImage(named: "dd1")!
+		
+		let song1 = Song(name: "I have been loved by you", photo: photo1, rating: 4)!
+		
+		let photo2 = UIImage(named: "dd2")!
+		let song2 = Song(name: "A ya syadu v kabriolet", photo: photo2, rating: 5)!
+		
+		let photo3 = UIImage(named: "dd3")!
+		let song3 = Song(name: "Singing Pugs Make It Again", photo: photo3, rating: 3)!
+		
+		songs += [song1, song2, song3]
+	}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+
+		return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		
+        return songs.count
+    }
+
+	
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		// Table view cells are reused and should be dequeued using a cell identifier.
+		let cellIdentifier = "SongTableViewCell"
+		let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! SongTableViewCell
+		
+		let song = songs[indexPath.row]
+		
+		cell.name.text = song.name
+		cell.photoImageView.image = song.photo
+		cell.ratingControl.rating = song.rating
+		
+		return cell
+	}
+	
+
+}
