46597105f72ad26174a63ca0447ace7350c4dd87
Ottavio Cambieri <Otto@MBP-di-Ottavio.lan>
2016-04-17 16:50:13 +0300

    Adding iOS project

---
+++ advanced Jukebox Swift/advanced Jukebox SwiftTests/advanced_Jukebox_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  advanced_Jukebox_SwiftTests.swift
+//  advanced Jukebox SwiftTests
+//
+//  Created by Ottavio Cambieri on 17/04/16.
+//  Copyright © 2016 advanced-jukebox-ios. All rights reserved.
+//
+
+import XCTest
+@testable import advanced_Jukebox_Swift
+
+class advanced_Jukebox_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
