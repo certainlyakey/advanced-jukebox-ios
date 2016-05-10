1845f8c2f66c6e0967bc9401e668c6729fbc076e
AleksandrBeliaev <aleksandr.beliaev@exove.ee>
2016-05-11 01:40:22 +0300

    added scripts to TV page

--- web-resources/scripts.js
+++ web-resources/scripts.js
@@ -1,27 +1,29 @@
-$(document).ready(function(){
-	var myFirebaseRef = new Firebase("https://radiant-torch-3216.firebaseio.com/");
-	$('.albumart').on('click',function() {
-		myFirebaseRef.set({
-			"song": {
-				"name": "Hulley Gulley set from JS",
-				"artist": "Beatles",
-				"album": "Twisterday"
-			}
-		});
-	});
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
 
-	myFirebaseRef.child("song/name").on("value", function(snapshot) {
-		$('#songname').text(snapshot.val());
+$(document).ready(function(){
+	var myFirebaseRef = new Firebase("https://radiant-torch-3216.firebaseio.com/songs");
+
+	myFirebaseRef.on("value", function(snapshot) {
+		var songs = snapshot.val();
+
+		songs.sort(function(a, b) {
+		    return parseFloat(a.votes) - parseFloat(b.votes);
+		}).reverse();
+
+		$('#playlist').empty();
+		$.each(songs, function(index, song) {
+			var listEl = '<li class="cf"><div class="playlist-votes">'+song.votes+'</div><img class="playlist-cover" src="'+song.imgurl+'" alt="'+song.name+'"><div class="playlist-name">'+song.name+'</div><div class="playlist-artist">'+song.artist+'</div></li>';
+			$('#playlist').append(listEl);
+		});
+		var currSong = songs[0];
+
+		$('#cover_current').attr('src', currSong.imgurl);
+		$('#song-album').text(currSong.album);
+		$('#song-artist').text(currSong.artist);
+		$('#song-name').text(currSong.name);
+		$('#current-song-votes').text(currSong.votes + ' votes');
+		
 	});
 });
-function modify_qty(val) {
-    var qty = document.getElementById('qty').value;
-    var new_qty = parseInt(qty,10) + val;
-    
-    if (new_qty < 0) {
-        new_qty = 0;
-    }
-    
-    document.getElementById('qty').value = new_qty;
-    return new_qty;
-}
